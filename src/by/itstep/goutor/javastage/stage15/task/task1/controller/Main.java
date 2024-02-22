a31939d2c83682165ff881378d4c00ee4a9821de
nikishkaa <nikita.goutor@gmail.com>
2024-02-22 20:30:55 +0300

    Add matrix init task1.

---
+++ src/by/itstep/goutor/javastage/stage15/task/task1/controller/Main.java
@@ -0,0 +1,9 @@
+package by.itstep.goutor.javastage.stage15.task.task1.controller;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/by/itstep/goutor/javastage/stage15/task/task1/util/MatrixInit.java
@@ -0,0 +1,15 @@
+package by.itstep.goutor.javastage.stage15.task.task1.util;
+
+public class MatrixInit {
+    public static int[][] getMatrix() {
+
+        int[][] matrix = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+
+        };
+
+        return matrix;
+    }
+}
