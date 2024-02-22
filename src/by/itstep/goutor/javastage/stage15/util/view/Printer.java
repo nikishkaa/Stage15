135ed70a6bf5750f49536368648d91630851625b
nikishkaa <nikita.goutor@gmail.com>
2024-02-22 20:28:26 +0300

    Initial commit

---
+++ src/by/itstep/goutor/javastage/stage15/util/view/Printer.java
@@ -0,0 +1,7 @@
+package by.itstep.goutor.javastage.stage15.util.view;
+
+public class Printer {
+    public static void print(StringBuilder builder) {
+        System.out.println(builder);
+    }
+}
