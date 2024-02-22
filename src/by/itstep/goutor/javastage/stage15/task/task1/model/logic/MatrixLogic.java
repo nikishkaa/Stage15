a704c8922c01954d58b1df9e37d890e49e8ef22d
nikishkaa <nikita.goutor@gmail.com>
2024-02-22 22:28:20 +0300

    Add protection in task 1.

--- src/by/itstep/goutor/javastage/stage15/task/task1/model/logic/MatrixLogic.java
+++ src/by/itstep/goutor/javastage/stage15/task/task1/model/logic/MatrixLogic.java
@@ -3,6 +3,10 @@ package by.itstep.goutor.javastage.stage15.task.task1.model.logic;
 public class MatrixLogic {
     public static int getSumMainDiagonal(int[][] matrix) {
 
+        if (matrix == null || matrix.length == 0) {
+            return -1;
+        }
+
         int sum = 0;
 
         for (int i = 0; i < matrix.length; i++) {
