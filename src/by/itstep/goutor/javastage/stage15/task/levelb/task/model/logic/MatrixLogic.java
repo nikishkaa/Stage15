f991596140d67fad360008f93b053fe72f7b8dba
nikishkaa <nikita.goutor@gmail.com>
2024-03-08 20:01:14 +0300

    Add first task level B stage 15.

--- src/by/itstep/goutor/javastage/stage15/task/levelb/task/model/logic/MatrixLogic.java
+++ src/by/itstep/goutor/javastage/stage15/task/levelb/task/model/logic/MatrixLogic.java
@@ -1,9 +1,22 @@
 package by.itstep.goutor.javastage.stage15.task.levelb.task.model.logic;
 
 public class MatrixLogic {
-
     public static int getCountEvenElementUpTheMainDiagonal(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return -1;
+        }
+
         int count = 0;
+
+        //O(N^2)
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix.length; j++) {
+                if (matrix[i][j] % 2 == 0) {
+                    count += 1;
+                }
+
+            }
+        }
 
 
         return count;
