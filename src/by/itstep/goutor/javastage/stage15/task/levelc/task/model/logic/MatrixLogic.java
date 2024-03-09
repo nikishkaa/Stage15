6e437b81da456f4f9b0fcb39f93231292e808153
nikishkaa <nikita.goutor@gmail.com>
2024-03-09 15:00:38 +0300

    Update Test. Update UI. Add task 1 level C.

--- src/by/itstep/goutor/javastage/stage15/task/levelc/task/model/logic/MatrixLogic.java
+++ src/by/itstep/goutor/javastage/stage15/task/levelc/task/model/logic/MatrixLogic.java
@@ -2,8 +2,19 @@ package by.itstep.goutor.javastage.stage15.task.levelc.task.model.logic;
 
 public class MatrixLogic {
     public static int getCountEvenElementUpTheMainDiagonal(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return -1;
+        }
 
         int countEvElementUpMainD = 0;
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (matrix[i][j] > 0) {
+                    countEvElementUpMainD++;
+                }
+            }
+        }
 
         return countEvElementUpMainD;
     }
